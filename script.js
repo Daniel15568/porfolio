f93dfae15718a850b882a20f7feca49b0845fd29
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2024-09-22 23:16:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links"); 
-    const icon = document.querySelector(".hamburger-icon");
+    const menu = document.querySelector(".menu-links");
     menu.classList.toggle("open");
-    icon.classList.toggle("open");
 }
+
+document.querySelector(".hamburger").addEventListener("click", toggleMenu);
