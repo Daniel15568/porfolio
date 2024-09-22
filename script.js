e2b51b964b442028bb78a428e6af10ff319f9e7e
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2024-09-22 23:05:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const darkModeButton = document.getElementById('toggle-dark-mode');
-darkModeButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
-        darkModeButton.textContent = 'Disable Dark Mode';
-    } else {
-        darkModeButton.textContent = 'Enable Dark Mode';
-    }
-});
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links"); 
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
