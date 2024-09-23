d5fabada60cea7f78bd6326ac6dcb7df8d94ddb4
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2024-09-23 23:12:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    menu.classList.toggle("open");
+    const menu = document.querySelector('.menu-links');
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+    menu.classList.toggle('open');
+
+    hamburgerIcon.classList.toggle('active');
 }
 
-document.querySelector(".hamburger").addEventListener("click", toggleMenu);
+document.querySelectorAll('.menu-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        const menu = document.querySelector('.menu-links');
+        const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+        menu.classList.remove('open');
+        hamburgerIcon.classList.remove('active');
+    });
+});
